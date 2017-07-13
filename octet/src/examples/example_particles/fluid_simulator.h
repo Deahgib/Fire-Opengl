79c04b283049cdced64beedf3efc0b71283c0319
MONSTER\Deahgib <louis.bennette@gmail.com>
2017-07-13 18:10:35 +0100

    Made fluid sim file for Jos Stam fluid simulation

---
+++ octet/src/examples/example_particles/fluid_simulator.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define IX(i, j) ((i) + (N+2)*(j))
+
+namespace octet {
+  /// Scene containing a particle system
+  class fluid_simulator {
+    float * u;
+
+  public:
+    fluid_simulator() {}
+  
+    void init() {
+
+    }
+  };
+
+}
